f9cf5077a85740aba676d60151125006d1c14513
Scott Humphries <scott@sscotth.io>
2015-06-05 16:07:14 -0500

    zip code lookup

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,47 @@
 angular
-  .module('weangular', [])
-  .controller('WeatherController', function ($http) {
+  .module('weangular', ['ngRoute'])
+
+  .config(function ($routeProvider) {
+      $routeProvider
+        .when('/', {
+          controller: 'WeatherController',
+          controllerAs: 'weather',
+          templateUrl: 'views/current.html'
+        })
+        .when('/:zip', {
+          controller: 'WeatherController',
+          controllerAs: 'weather',
+          templateUrl: 'views/current.html'
+        });
+  })
+
+  .controller('WeatherController', function ($http, $routeParams) {
     var vm = this;
 
-    navigator.geolocation.getCurrentPosition(function (geoposition) {
-      var lat = geoposition.coords.latitude;
-      var long = geoposition.coords.longitude;
+    vm.zip = +$routeParams.zip;
 
+    if (vm.zip) {
       $http
-        .get(`http://api.wunderground.com/api/4887e91c6c12fd6e/conditions/q/${lat},${long}.json`)
+        .get(`http://api.wunderground.com/api/4887e91c6c12fd6e/conditions/q/${vm.zip}.json`)
         .success(function (data) {
           vm.temp_f = data.current_observation.temp_f;
         });
+    } else {
+      navigator.geolocation.getCurrentPosition(function (geoposition) {
+        var lat = geoposition.coords.latitude;
+        var long = geoposition.coords.longitude;
 
-    });
+        $http
+          .get(`http://api.wunderground.com/api/4887e91c6c12fd6e/conditions/q/${lat},${long}.json`)
+          .success(function (data) {
+            vm.temp_f = data.current_observation.temp_f;
+          });
+
+      });
+    }
+
+    vm.zipLookup = function () {
+      window.location = '/#/' + vm.zip;
+    };
+
   });
