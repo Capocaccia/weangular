be6edfd8c29536e1a1b5e5d999623f4a08fe7e0e
Scott Humphries <scott@sscotth.io>
2015-06-05 15:33:35 -0500

    geolocated current conditions

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+angular
+  .module('weangular', [])
+  .controller('WeatherController', function ($http) {
+    var vm = this;
+
+    navigator.geolocation.getCurrentPosition(function (geoposition) {
+      var lat = geoposition.coords.latitude;
+      var long = geoposition.coords.longitude;
+
+      $http
+        .get(`http://api.wunderground.com/api/4887e91c6c12fd6e/conditions/q/${lat},${long}.json`)
+        .success(function (data) {
+          vm.temp_f = data.current_observation.temp_f;
+        });
+
+    })
+  });
